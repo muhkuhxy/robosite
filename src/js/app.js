e957a6b8e5b2cd466a10313487d617b42f021793
muhkuhxy <>
2017-08-02 22:20:46 +0200

    start mobile optimizing navbar

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,18 @@
 import {decodeAll} from './decode'
 import * as contact from './contact'
 import './polyfills'
+import { $ } from './dom-utils'
 
 document.body.classList.remove('no-js')
+
+var nav = $('.navbar__items')
+$('.navbar__burger').addEventListener('click', function() {
+  if (nav.classList.contains('closed')) {
+    nav.classList.remove('closed')
+  } else {
+    nav.classList.add('closed')
+  }
+})
 
 decodeAll()
 contact.bind()
