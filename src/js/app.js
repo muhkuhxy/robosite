0dcaa71a7c50037817954a855a0ee63a539b04c9
muhkuhxy <>
2017-08-03 15:14:32 +0200

    responsify footer

--- src/js/app.js
+++ src/js/app.js
@@ -2,23 +2,8 @@ import {decodeAll} from './decode'
 import * as contact from './contact'
 import './polyfills'
 import * as menu from './menu'
-import { $ } from './dom-utils'
 
 document.body.classList.remove('no-js')
-
-// var img = $('.header__image img')
-// function focalize () {
-//   return
-//   var ratio = window.innerWidth / (window.innerHeight * 0.6)
-//   console.log(ratio)
-//   var margin = ratio * 10
-//   console.log('ratio * 85px = ' + margin)
-//   img.style.marginTop = -margin + '%'
-// }
-// window.addEventListener('resize', function () {
-//   window.requestAnimationFrame(focalize)
-// })
-// window.requestAnimationFrame(focalize)
 
 decodeAll()
 contact.bind()
