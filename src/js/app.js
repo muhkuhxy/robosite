820c003efcb91904efa389cd832508c3f92d3a48
muhkuhxy <>
2017-08-03 09:44:07 +0200

    hide nav menu behind navbar

--- src/js/app.js
+++ src/js/app.js
@@ -6,13 +6,18 @@ import { $ } from './dom-utils'
 document.body.classList.remove('no-js')
 
 var nav = $('.navbar__items')
-$('.navbar__burger').addEventListener('click', function () {
+function toggleMenu (e) {
+  console.log('called')
   if (nav.classList.contains('closed')) {
     nav.classList.remove('closed')
+    document.body.addEventListener('click', toggleMenu)
   } else {
     nav.classList.add('closed')
+    document.body.removeEventListener('click', toggleMenu)
   }
-})
+  e.stopPropagation()
+}
+$('.navbar__burger').addEventListener('click', toggleMenu)
 
 decodeAll()
 contact.bind()
